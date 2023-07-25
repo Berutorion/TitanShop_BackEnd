3b479002ada6e9379498101767461254b2dc97ee
Eagle1630 <benson.pang@technine.io>
2023-07-25 16:37:36 +0800

    prepare to deploy

--- config/config.js
+++ config/config.js
@@ -19,9 +19,9 @@ module.exports = {
     timezone: '+08:00',
   },
   production: {
-    username: process.env.DB_USER,
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: 'mysql',
     timezone: '+08:00',
